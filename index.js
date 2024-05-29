44b7fd36cb08c0e30fbb07dd29a7a33f75085c17
Yashodhan Advankar <yashodhan.advankar@profound.works>
2024-05-29 13:15:37 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to simulate delay
+function delayMiddleware(req, res, next) {
+    setTimeout(next, 2000); // Simulating 2 seconds delay
+}
+
+// Endpoint that responds quickly
+app.get('/fast', (req, res) => {
+    res.send('This is the fast endpoint');
+});
+
+// Endpoint that responds after some delay
+app.get('/slow', delayMiddleware, (req, res) => {
+    res.send('This is the slow endpoint');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+});
